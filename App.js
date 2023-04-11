b6ecab8e2a29a96631205401c0f8bea7920d8c6d
Bailey Cage <bailey.cage@gmail.com>
2023-04-11 14:18:43 -0400

    get forecast from API; clean up fetch functions

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // React & related
 import React, { useState } from 'react'
-import { Alert, View } from 'react-native'
+import { Alert, SafeAreaView, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
@@ -11,6 +11,7 @@ import WeatherDisplay from './src/components/WeatherDisplay'
 // Misc
 import { API_KEY } from './keys.js'
 import STYLES from './App.scss'
+import { LinearGradient } from 'expo-linear-gradient'
 
 const ZIP_FORMAT = /^[0-9]{5}$/
 const Stack = createNativeStackNavigator()
@@ -19,6 +20,7 @@ const Stack = createNativeStackNavigator()
 export default function App() {
   const [ zip, setZip ] = useState('')
   const [ weather, setWeather ] = useState(null)
+  const [ forecast, setForecast ] = useState(null)
   const [ colors, setColors ] = useState(['#87c1ff', '#8ed5ff', '#75b0fe'])
 
   function clearData () {
@@ -35,33 +37,31 @@ export default function App() {
   async function getWeather (zipCode = zip) {
     return getLoc(zipCode)
       .then(geoloc=>{
-        if (!!geoloc && Object.keys(geoloc).includes('lat')) {
-          return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geoloc.lat}&lon=${geoloc.lon}&appid=${API_KEY}&units=imperial`)
-            .then(r=>{
-              if (Object.keys(r).includes('ok') && r.ok) return r.json().then(data => {
-                data = {...data, ok: true, zipCode: zipCode, iconUrl: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-                setWeather(data)
-                return data
-              })
-              else return r.json()
+        if (!!geoloc && !!geoloc.lat) {
+          return getCurrWeather(geoloc, setWeather)
+            .then(r => {
+              return getForecast(geoloc, setForecast)
             })
-        } else if (!!geoloc && Object.keys(geoloc).includes('message')) {
+        } else if (!!geoloc && !!geoloc.message) {
           return Alert.alert('invalid zip code', 'zip code does not exist.')
         }
       })
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='home'>
-          {(props) => <HomeScreen {...props} zip={zip} setZip={evalZip} getWeather={getWeather} colors={colors} />}
-        </Stack.Screen>
-        <Stack.Screen name='weather'>
-          {(props) => <WeatherScreen {...props} zip={zip} weather={weather} clearData={clearData} colors={colors} setColors={setColors} />}
-        </Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <React.Fragment>
+      <LinearGradient colors={colors} style={STYLES.background} />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='home'>
+            {(props) => <HomeScreen {...props} zip={zip} setZip={evalZip} getWeather={getWeather} colors={colors} />}
+          </Stack.Screen>
+          <Stack.Screen name='weather'>
+            {(props) => <WeatherScreen {...props} zip={zip} weather={weather} forecast={forecast} clearData={clearData} colors={colors} setColors={setColors} />}
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </React.Fragment>
   )
 }
 
@@ -88,8 +88,30 @@ function WeatherScreen (props) {
 
 async function getLoc (zipCode) {
   if (ZIP_FORMAT.test(zipCode)) {
-    return fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode},US&appid=${API_KEY}`).then(r => {return r.json()})
+    return fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode},US&appid=${API_KEY}`).then(r=>{ return r.json() })
   } else {
     return Alert.alert('invalid zip code', 'zip code must be 5 digits.')
   }
+}
+
+async function getCurrWeather (geoloc, setWeather) {
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geoloc.lat}&lon=${geoloc.lon}&appid=${API_KEY}&units=Imperial`)
+    .then(r=>{
+      if (!!r && r.ok) return r.json().then(data => {
+        setWeather(data)
+        return {...data, ok: true}
+      })
+      else return {...r.json(), ok: false}
+    })
+}
+
+async function getForecast (geoloc, setForecast) {
+  return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${geoloc.lat}&lon=${geoloc.lon}&appid=${API_KEY}&units=Imperial`)
+    .then(r=>{
+      if (!!r && r.ok) return r.json().then(data => {
+        setForecast(data)
+        return {...data, ok: true}
+      })
+      else return {...r.json(), ok: false}
+    })
 }
