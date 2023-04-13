06294d3f8f180ae9f8c117e9a670246697783232
Bailey Cage <bailey.cage@gmail.com>
2023-04-13 12:03:40 -0400

    display current weather instead of first forecast

--- src/components/LocationMiniCard.jsx
+++ src/components/LocationMiniCard.jsx
@@ -8,11 +8,16 @@ import { API_KEY } from '../../keys'
 const LocationMiniCard = (props) => {
   const { loc, styles, getWeather, navigation } = props
   const [ forecast, setForecast ] = useState({})
+  const [ weather, setWeather ] = useState({})
 
   useEffect(() => {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${loc.lat}&lon=${loc.lon}&appid=${API_KEY}&units=Imperial`)
       .then(r=>{
         if (!!r && r.ok) r.json().then(setForecast)
+      })
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${loc.lat}&lon=${loc.lon}&appid=${API_KEY}&units=Imperial`)
+      .then(r=>{
+        if (!!r && r.ok) r.json().then(setWeather)
       })
   }, [loc])
 
@@ -20,16 +25,16 @@ const LocationMiniCard = (props) => {
     getWeather(loc).then(() => navigation.navigate('weather'))
   }
 
-  if (!forecast || !forecast.list) return <></>
+  if (!weather || !forecast || !forecast.list) return <></>
   return (
     <Card containerStyle={{margin: 0}}>
       <TouchableOpacity onPress={handleSelect}>
         <View style={{flexDirection: 'column', alignItems: 'center'}}>
-          <Image style={styles.weatherIconMini} source={{uri: `https://openweathermap.org/img/wn/${forecast.list[0].weather[0].icon}@2x.png`}}/>
+          <Image style={styles.weatherIconMini} source={{uri: `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}}/>
           <Text style={styles.h4}>{forecast.city.name}</Text>
         </View>
         <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly'}}>
-          <Text style={styles.currTempMini}>{`${Math.floor(forecast.list[0].main.temp)}\u00B0`}</Text>
+          <Text style={styles.currTempMini}>{`${Math.floor(weather.main.temp)}\u00B0`}</Text>
           <View>
             <View style={{flexDirection: 'row'}}>
               <MaterialCommunityIcons name="weather-rainy" size={16} color="black" style={styles.miniIcon} />
@@ -37,7 +42,7 @@ const LocationMiniCard = (props) => {
             </View>
             <View style={{flexDirection: 'row'}}>
               <MaterialCommunityIcons name="weather-windy" size={16} color="black" style={styles.miniIcon} />
-              <Text>{`${Math.round(forecast.list[0].wind.speed)} mph`}</Text>
+              <Text>{`${Math.round(weather.wind.speed)} mph`}</Text>
             </View>
           </View>
         </View>
